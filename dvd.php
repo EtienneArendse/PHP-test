1c6258490e441b11c3b7833de88bbef0d0661a40
Etienne Arendse <etienne@limabean.co.za>
2017-10-09 15:03:37 +0200

    task-8-e done

---
+++ dvd.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      .dvdAnchor:link    {color:orange;}
+      .dvdAnchor:visited {color:green;}
+      .dvdAnchor:hover   {color:yellow;}
+      .dvdAnchor:active  {color:red;}
+    </style>
+  </head>
+
+  <body>
+    <table style="width:100%">
+      <tr>
+        <th>id</th>
+        <th>name</th> 
+        <th>description</th>
+        <th>release_date</th>
+        <th>category</th>
+      </tr>
+      <?php if(!empty($result_set)): ?>
+      <?php foreach($result_set as $dvd): ?>
+      <tr>
+        <td>
+          <?php
+          echo $dvd['id'];
+          ?>
+          </td>
+        <td>
+          <?php
+          echo $dvd['name'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $dvd['description'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $dvd['release_date'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $dvd['category'];
+          ?>
+        </td>
+
+        <td>
+        <form action="update_dvd.php" method="get">
+        <input type="hidden" name="id" value="<?php echo $dvd['id']; ?>">
+        <input type="submit" value="edit">
+        </form>
+
+        <form action="delete_dvd.php" method="get">
+        <input type="hidden" name="id" value="<?php echo $dvd['id']; ?>">
+        <input type="submit" value="delete">
+        </form>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+      <?php endif; ?>
+    </table>
+
+    <a href="add_dvd_front.php" class="dvdAnchor">Add DVD</a>
+  </body>
+</html>
